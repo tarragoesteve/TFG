4cef34c367793dc8edcf04ff3bfa3638cf6219b4
esteve <tarragoesteve@gmail.com>
2018-02-26 11:41:50 -0500

    first trial working without shapes and without weights

--- exponents.py
+++ exponents.py
@@ -1,16 +1,19 @@
 import numpy as np
 
-def exponents(variables, degree):
+def exactdegree(variables, degree):
     if degree == 0:
         return [np.repeat(0,variables).tolist()]
     if variables == 0:
         return [];
     ret = []
     for newdegree in range(0,degree+1):
-        for l in exponents(variables-1, newdegree):
+        for l in exactdegree(variables-1, newdegree):
             l.append(degree-newdegree)
             ret.append(l)
     return ret;
 
-
-print( exponents(3,2) )
+def uptodegree(variables, degree):
+    ret = []
+    for i in range(0,degree+1):
+        ret.extend(exactdegree(variables,i))
+    return ret
--- polynomiallayer.py
+++ polynomiallayer.py
@@ -3,26 +3,56 @@ from __future__ import division
 from __future__ import print_function
 from sympy.polys.monomials import monomial_count
 
+import exponents
 import numpy as np
 import tensorflow as tf
 
-
 ##parameters
-degree = 5;
-variables = 25;
-print( monomial_count(variables, degree))
-
-##
-input = tf.placeholder(tf.float32);
-coeficients =  tf.get_variable("coeficients", [1, 2, 3]);
+degree = 10;
+variables = 3;
 
 
-a = tf.constant(3.0, dtype=tf.float32)
-b = tf.constant(4.0)
-total = input
+#print( monomial_count(variables, degree))
 
 
+#calculating the sparce matrix from the exponents
+exponent = exponents.uptodegree(variables,degree)
+sparcematrix = [];
+for i in range(variables):
+    sparcematrix.append(np.zeros(((degree+1), len(exponent)),dtype=np.dtype('float32')))
+    for j in range(len(exponent)):
+        a = exponent[j][i]
+        sparcematrix[i][a][j] = np.float32(1.0)
 
+
+#calculating all the power of input up to degree
+input = tf.placeholder(tf.float32)
+power = [tf.constant(np.repeat(1, variables), tf.float32)]
+
+for _ in range(degree):
+    power.append(tf.multiply(power[len(power)-1], input))
+
+transposedpower = tf.transpose(power)
+
+
+singlepowers = []
+for i in range(variables):
+    singlepowers.append(tf.slice(transposedpower,[i, 0],[1,degree+1]))
+
+
+vectors = []
+result = np.repeat(1.0, len(exponent),)
+for i in range(variables):
+    aux = tf.matmul(singlepowers[i],sparcematrix[i])
+    result = aux * result;
+    vectors.append(aux)
+
+#w = tf.get_variable("weights")
+#result = w * result
+
+
+total = tf.reduce_sum(result);
+
+print("Graph defined")
 sess = tf.Session()
-
-print(sess.run(total, feed_dict={input: [1, 3, 4 , 5]}))
+print(sess.run(total, feed_dict={input: [1, 2, 3]}))
