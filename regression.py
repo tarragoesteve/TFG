765564ef85148852381e669a6a39877b6173d08b
esteve <tarragoesteve@gmail.com>
2018-02-27 15:27:28 -0500

    Deleting unused code

--- regression.py
+++ regression.py
@@ -1,15 +1,11 @@
 '''
-A linear regression learning algorithm example using TensorFlow library.
-
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
+Author: Esteve Tarrago
 '''
 
 from __future__ import print_function
 
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 from polylayer import PolyLayer
 
 # Parameters
@@ -67,27 +63,3 @@ with tf.Session() as sess:
                 print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), "weights= ",sess.run(tf.get_variable("weights",use_resource=True)) )
     print("Optimization Finished!")
     training_cost = sess.run(loss, feed_dict={X: train_X, Y: train_Y})
-    #print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
-
-    # Graphic display
-    plt.plot(train_X, train_Y, 'ro', label='Original data')
-    #plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    #plt.legend()
-    #plt.show()
-
-    # Testing example, as requested (Issue #2)
-    #test_X = numpy.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
-    #test_Y = numpy.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
-
-    #print("Testing... (Mean square loss Comparison)")
-    #testing_cost = sess.run(
-    #    tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
-    #    feed_dict={X: test_X, Y: test_Y})  # same function as cost above
-    # print("Testing cost=", testing_cost)
-    # print("Absolute mean square loss difference:", abs(
-    #     training_cost - testing_cost))
-    #
-    # plt.plot(test_X, test_Y, 'bo', label='Testing data')
-    # plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    # plt.legend()
-    # plt.show()
