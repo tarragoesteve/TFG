a0618d225c32369347d994f84ce9eefded5e95cd
esteve <tarragoesteve@gmail.com>
2018-02-27 12:16:32 -0500

    Traceback (most recent call last):   File "/snap/pycharm-professional/47/helpers/pydev/pydev_run_in_console.py", line 53, in run_file     pydev_imports.execfile(file, globals, locals)  # execute the script   File "/home/esteve/PycharmProjects/TFG/regression.py", line 36, in <module>     pred = mylayer.call(inputs=X)   File "/home/esteve/PycharmProjects/TFG/polylayer.py", line 16, in call     input = tf.reshape(inputs, [None, self._variables])   File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/ops/gen_array_ops.py", line 3997, in reshape     "Reshape", tensor=tensor, shape=shape, name=name)   File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/framework/op_def_library.py", line 513, in _apply_op_helper     raise err TypeError: Failed to convert object of type <type 'list'> to Tensor. Contents: [None, 3]. Consider casting elements to a supported type.

--- polylayer.py
+++ polylayer.py
@@ -6,42 +6,42 @@ import exponents
 
 class PolyLayer(base.Layer):
     def __init__(self, variables, degree=3):
-        self.variables =  variables
-        self.degree = degree
+        self._variables =  variables
+        self._degree = degree
 
     def build(self, _):
         pass
 
     def call(self, inputs, **kwargs):
-        input = tf.placeholder(tf.float32, [None, self.variables])
+        input = tf.reshape(inputs, [None, self._variables])
 
         # calculating the sparce matrix from the exponents
-        exponent = exponents.uptodegree(self.variables, self.degree)
-        sparcematrix = [];
-        for i in range(self.variables):
-            sparcematrix.append(np.zeros(((self.degree + 1), len(exponent)), dtype=np.dtype('float32')))
-            for j in range(len(exponent)):
-                a = exponent[j][i]
-                sparcematrix[i][a][j] = np.float32(1.0)
+        self._exponent = exponents.uptodegree(self._variables, self._degree)
+        self._sparcematrix = [];
+        for i in range(self._variables):
+            self._sparcematrix.append(np.zeros(((self._degree + 1), len(self._exponent)), dtype=np.dtype('float32')))
+            for j in range(len(self._exponent)):
+                a = self._exponent[j][i]
+                self._sparcematrix[i][a][j] = np.float32(1.0)
 
         def work(batch):
             # calculating all the power of input up to degree
-            power = [tf.constant(np.repeat(1, self.variables), tf.float32)]
-            for _ in range(self.degree):
+            power = [tf.constant(np.repeat(1, self._variables), tf.float32)]
+            for _ in range(self._degree):
                 power.append(tf.multiply(power[len(power) - 1], batch))
 
             # transpose and slice
             transposedpower = tf.transpose(power)
             singlepowers = []
-            for i in range(self.variables):
-                singlepowers.append(tf.slice(transposedpower, [i, 0], [1, degree + 1]))
+            for i in range(self._variables):
+                singlepowers.append(tf.slice(transposedpower, [i, 0], [1, self._degree + 1]))
 
             # compute monomials
-            result = np.repeat(1.0, len(exponent))
-            for i in range(self.variables):
-                result = result * tf.matmul(singlepowers[i], sparcematrix[i])
+            result = np.repeat(1.0, len(self._exponent))
+            for i in range(self._variables):
+                result = result * tf.matmul(singlepowers[i], self._sparcematrix[i])
 
-            w = tf.get_variable("weights", [len(exponent)], dtype=tf.float32, initializer=tf.initializers.random_normal)
+            w = tf.get_variable("weights", [len(self._exponent)], dtype=tf.float32, initializer=tf.initializers.random_normal)
             result = w * result
 
             # sum monomials
@@ -50,3 +50,7 @@ class PolyLayer(base.Layer):
         output = tf.map_fn(work, input)
 
         return output
+
+    def layer(self, input, variables, degree=3):
+        self.__init__(variables=variables,degree=degree)
+        return self.call(input)
---
+++ regression.py
@@ -0,0 +1,87 @@
+'''
+A linear regression learning algorithm example using TensorFlow library.
+
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from polylayer import PolyLayer
+
+# Parameters
+learning_rate = 0.01
+training_epochs = 1000
+display_step = 50
+batch_size = 2
+variables = 3
+
+# Training Data
+x = np.asarray(np.random.rand(1000, variables))
+y = np.asarray( (lambda x: pow(x[0], 2) * x[1] * pow(x[2], 3), x))
+train_X = x[0:500]
+train_Y = y[0:500]
+eval_X = x[500:1000]
+eval_Y = y[500:1000]
+
+n_samples = train_X.shape[0]
+
+
+X = tf.placeholder(tf.float32, [None,variables])
+Y = tf.placeholder(tf.float32, [None,1])
+mylayer = PolyLayer(variables,2)
+pred = mylayer.call(inputs=X)
+# Mean squared error
+
+loss = tf.losses.mean_squared_error(Y, pred)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss=loss)
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+
+# Start training
+with tf.Session() as sess:
+
+    # Run the initializer
+    sess.run(init)
+
+    # Fit all training data
+    for epoch in range(training_epochs):
+        for batch in range(n_samples/batch_size):
+            X_batch = tf.reshape(train_X[batch_size*batch:batch_size*(batch+1)], [None, variables])
+            Y_batch = tf.reshape(train_Y[batch_size*batch:batch_size*(batch+1)], [None, 1])
+            sess.run(optimizer,feed_dict={X: X_batch, Y: Y_batch})
+        # Display logs per epoch step
+        if (epoch+1) % display_step == 0:
+            c = sess.run(loss, feed_dict={X: train_X, Y:train_Y})
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c))
+
+    print("Optimization Finished!")
+    training_cost = sess.run(loss, feed_dict={X: train_X, Y: train_Y})
+    #print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+
+    # Graphic display
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    #plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    #plt.legend()
+    #plt.show()
+
+    # Testing example, as requested (Issue #2)
+    #test_X = numpy.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
+    #test_Y = numpy.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
+
+    #print("Testing... (Mean square loss Comparison)")
+    #testing_cost = sess.run(
+    #    tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
+    #    feed_dict={X: test_X, Y: test_Y})  # same function as cost above
+    # print("Testing cost=", testing_cost)
+    # print("Absolute mean square loss difference:", abs(
+    #     training_cost - testing_cost))
+    #
+    # plt.plot(test_X, test_Y, 'bo', label='Testing data')
+    # plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    # plt.legend()
+    # plt.show()
