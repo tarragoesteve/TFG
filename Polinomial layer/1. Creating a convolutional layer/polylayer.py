b3aff4f503d232e6d8d1235fe3ac3547d89b13bb
redelweisser <ets1995@hotmail.com>
2018-02-28 20:03:37 -0500

    Computing lots of filters at the same time

--- Polinomial layer/1. Creating a convolutional layer/polylayer.py
+++ Polinomial layer/1. Creating a convolutional layer/polylayer.py
@@ -5,7 +5,7 @@ import exponents
 
 
 class Conv2DPolynomial(base.Layer):
-    def __init__(self, degree=1,filters=32,kernel_size=[3, 3],channels =1,
+    def __init__(self, degree=1,filters=3,kernel_size=[3, 3],channels =1,
                  padding="same",activation=tf.nn.relu):
         self._variables = kernel_size[1] * kernel_size[0] * channels
         self._channels = channels
@@ -20,6 +20,9 @@ class Conv2DPolynomial(base.Layer):
         self._input_height = 3
         self._exponent = exponents.uptodegree(self._variables, self._degree)
         self._sparcematrix = [];
+        self._weights = [];
+        for i in range(self._filters):
+            self._weights.append(tf.get_variable("w"+str(i), [len(self._exponent)], dtype=tf.float32, initializer=tf.random_normal_initializer))
         for i in range(self._variables):
             self._sparcematrix.append(np.zeros(((self._degree + 1), len(self._exponent)), dtype=np.dtype('float32')))
             for j in range(len(self._exponent)):
@@ -36,7 +39,7 @@ class Conv2DPolynomial(base.Layer):
         if y >= self._input_height: return False
         return True
 
-    def _compute_filter(self, input,x,y,w):
+    def _compute_filter(self, input,x,y):
         variables = []
         for i in range(x-self._kernel_size[1]/2,x+1+self._kernel_size[1]/2):
             for j in range(y-self._kernel_size[0]/2,y+1+self._kernel_size[0]/2):
@@ -60,19 +63,19 @@ class Conv2DPolynomial(base.Layer):
         result = np.repeat(1.0, len(self._exponent))
         for i in range(self._variables):
             result = result * tf.matmul(singlepowers[i], self._sparcematrix[i])
-        result = w * result
-        # sum monomials
-        return tf.reduce_sum(result)
+        ret = []
+        for w_variable in self._weights:
+            ret.append(tf.reduce_sum(result*w_variable))
+        return ret
 
 
     def call(self, input, **kwargs):
         aux = []
-        w = tf.get_variable("weigh", [len(self._exponent)], dtype=tf.float32, initializer=tf.random_normal_initializer)
         for i in range(self._final_height):
             for j in range(self._final_width):
-                aux.append(self._compute_filter(input, i, j, w))
+                aux.append(self._compute_filter(input, i, j))
 
-        output = tf.reshape(aux, [self._final_height,self._final_width])
+        output = tf.reshape(aux, [self._final_height,self._final_width,self._filters])
         return output
 
 
