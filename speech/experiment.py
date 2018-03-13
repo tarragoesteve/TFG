864880e3eb6f6c580216bb41cd869d2aef5bd08f
redelweisser <ets1995@hotmail.com>
2018-03-13 16:56:58 -0400

    saving csv

--- speech/experiment.py
+++ speech/experiment.py
@@ -1,20 +1,47 @@
 from scipy.io import wavfile
 import gabor
-
+from numba import jit
 import numpy as np
+import time
+
+
+start = time.time()
+
 stripe = 20000
-filter_size=20000
+filter_size= 20000
+
+#rate samples/sec
+rate, data = wavfile.read("./Data/a.wav")
+
+lamb = []
+theta = []
+phi = []
+sigma = []
+gamma = []
+
+@jit
+def compute(input,filter):
+    return np.sum(np.dot(filter,input))
 
 #rate samples/sec
 
 #ffmpeg -i ./1520881828282.mp3 a.wav
-rate, data = wavfile.read("/home/esteve/PycharmProjects/TFG/speech/Data/a.wav")
-#rate = 44100
+
 #lamb, theta, phi, sigma, gamma, input
+start = time.time()
+filters = []
+filters.append(gabor.gabor_1D(1,1,1,1,1,filter_size))
+end = time.time()
+print(end - start)
+
+start = time.time()
 ret = []
 for i in range(len(data)/stripe):
     input = data[i*stripe:(i+1)*stripe]
-    ret.append(gabor.gabor_1D(1,1,1,1,1,input))
+    for j in range(len(filters)):
+        ret.append(compute(filters[j],input))
     if (i % 100 == 0): print(str(i) + "/" + str(len(data)/stripe))
 print(ret)
+end = time.time()
+print(end - start)
 
--- speech/gabor.py
+++ speech/gabor.py
@@ -3,13 +3,13 @@ import math
 import numpy as np
 
 
-def gabor_1D(lamb, theta, phi, sigma, gamma, input):
-    ret = 0
-    for i in range(len(input)):
+def gabor_1D(lamb, theta, phi, sigma, gamma, input_size):
+    ret = []
+    for i in range(input_size):
         x = i
         x_prim = x * cmath.cos(theta)
         y_prim = -x * cmath.sin(theta)
         g = cmath.exp(-(x_prim*x_prim+ gamma*gamma * y_prim*y_prim)/2*sigma*sigma)*cmath.cos(2*math.pi*x_prim/lamb + phi)
-        ret += g * input[i]
+        ret.append(g)
     return ret
 
